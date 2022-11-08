29a1cbec3cccd89a8ee24f2951b18a65f3d79161
ikurotime <davidhuertasortiz@gmail.com>
2022-11-08 21:49:47 +0100

    Week 1 🚀

---
+++ README.md
@@ -0,0 +1,24 @@
+# ¡Codember en rust!
+
+Este repositorio contiene el código con las soluciones de los ejercicios de la página web de [Codember](https://codember.dev) en rust.
+Cada semana se publica un nuevo ejercicio y se puede resolver en cualquier lenguaje de programación.
+
+## Ejercicios
+
+[Semana 1](https://github.com/ikurotime/codember_rust/Week%201/)
+
+## Ejecutar los ejercicios
+
+Cada ejercicio se puede ejecutar con el comando `./main` en la carpeta del ejercicio.
+
+## Contribuir
+
+Tienes otra solución para un ejercicio? Puedes hacer un pull request con tu solución.
+Crea una carpeta con tu nombre de usuario de github y añade tu solución en un archivo `main.rs`.
+Por ejemplo si tu nombre de usuario es `ikurotime` y quieres añadir una solución para el ejercicio de la semana 1, crea una carpeta `ikurotime` en la carpeta `Week 1` y añade tu solución en un archivo `main.rs`.
+De forma que la estructura de carpetas quede así:
+
+    Week 1
+    ├── ikurotime
+    │   └── main.rs
+    └── README.md
---
+++ Week 1/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+
+fn main() {
+    println!("In file {}", "users.txt");
+    let contents = fs::read_to_string("users.txt")
+        .expect("Should have been able to read the file");
+        //Create new empty array
+    let mut users: Vec<String> = Vec::new();
+    contents.lines().for_each(|line| {
+        //if line is empty sustitute with "|" and push to array, else push line to array
+        if line.trim().is_empty() {
+          users.push("|".to_string());
+        } else {
+          //si la linea no contiene @midudev pushea al array
+          users.push(line.to_string());
+        }
+    });
+    let binding = users.join(" ");
+    let users = binding.split("|").collect::<Vec<&str>>();    
+    let mut valid_users: Vec<String> = Vec::new();
+    //Iterate over users array and push to valid_users array if it fits the requirements
+    for user in users {
+      if user.contains("usr:") && user.contains("eme:") && user.contains("psw:") && user.contains("age:") && user.contains("loc:") && user.contains("fll:")  {
+        valid_users.push(user.to_string());
+      }
+    }
+    println!("Last valid user: {}", valid_users[valid_users.len() - 1].to_string());
+    println!("Number of valid users: {}", valid_users.len());
+    
+}
